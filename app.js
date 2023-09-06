9518d6d28603cc2d9a1a57e4e7dda569301f784e
yonatan taub <yonatantaub36@gmail.com>
2023-09-06 10:51:50 +0300

    addTodo

---
+++ app.js
@@ -0,0 +1,46 @@
+//Selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+//Event Listeners
+todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
+
+//Functions
+
+function addTodo(event) {
+  event.preventDefault();
+
+  //Todo DIV
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+
+  //Todo LI
+  const newTodo = document.createElement("li");
+  newTodo.innerText = todoInput.value;
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
+
+  //CHECK MARK BUTTON
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = '<i class = "fas fa-check"></i>';
+  completedButton.classList.add("complete-btn");
+  todoDiv.appendChild(completedButton);
+
+  //CHECK trash BUTTON
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = '<i class = "fas fa-trash"></i>';
+  trashButton.classList.add("trash-btn");
+  todoDiv.appendChild(trashButton);
+
+  //APPEND TO LIST
+  todoList.appendChild(todoDiv);
+
+  //Clear Todo INPUT VALUE
+  todoInput.value = "";
+}
+
+function deleteCheck(event) {
+  event.preventDefault();
+}
