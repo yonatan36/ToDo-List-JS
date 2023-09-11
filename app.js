1931955132ea6d4b78cdaf2a4a83513b906f95bd
yonatan taub <yonatantaub36@gmail.com>
2023-09-11 09:40:13 +0300

    local storage

--- app.js
+++ app.js
@@ -2,16 +2,17 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
 
 //Event Listeners
+document.addEventListener("DOMContentLoaded", getTodos);
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteCheck);
+filterOption.addEventListener("click", filterTodo);
 
-//Functions
-
+//Function
 function addTodo(event) {
   event.preventDefault();
-
   //Todo DIV
   const todoDiv = document.createElement("div");
   todoDiv.classList.add("todo");
@@ -21,6 +22,7 @@ function addTodo(event) {
   newTodo.innerText = todoInput.value;
   newTodo.classList.add("todo-item");
   todoDiv.appendChild(newTodo);
+  saveLocalTodos(todoInput.value);
 
   //CHECK MARK BUTTON
   const completedButton = document.createElement("button");
@@ -42,5 +44,114 @@ function addTodo(event) {
 }
 
 function deleteCheck(event) {
-  event.preventDefault();
+  const item = event.target;
+
+  if (item.classList[0] === "trash-btn") {
+    const todo = item.parentElement;
+    const todoText = todo.children[0].innerText; // Get the text value of the to-do
+    // Animation
+    todo.classList.add("fall");
+    todo.addEventListener("transitionend", function () {
+      todo.remove();
+      // Remove the specific value from local storage
+      removeLocalTodos(todoText);
+    });
+  }
+  //CHECK MARK
+  if (item.classList[0] === "complete-btn") {
+    const todo = item.parentElement;
+    todo.classList.toggle("completed");
+  }
+}
+
+function filterTodo(event) {
+  const todos = Array.from(todoList.childNodes).filter(
+    (node) => node.nodeType === Node.ELEMENT_NODE
+  );
+
+  todos.forEach(function (todo) {
+    switch (event.target.value) {
+      case "all":
+        todo.style.display = "flex";
+        break;
+      case "completed":
+        if (todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+      case "uncompleted":
+        if (!todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+    }
+  });
+}
+
+function saveLocalTodos(todo) {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  todos.push(todo);
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function getTodos() {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  todos.forEach(function (todo) {
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+
+    //Todo LI
+    const newTodo = document.createElement("li");
+    newTodo.innerText = todo;
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
+
+    //CHECK MARK BUTTON
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = '<i class = "fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+
+    //CHECK trash BUTTON
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = '<i class = "fas fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+
+    //APPEND TO LIST
+    todoList.appendChild(todoDiv);
+  });
+}
+
+function removeLocalTodos(todoText) {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+
+  // Find the index of the to-do item with the matching text
+  const todoIndex = todos.indexOf(todoText);
+
+  if (todoIndex !== -1) {
+    // Remove the specific value from the array
+    todos.splice(todoIndex, 1);
+    // Update local storage
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }
 }
